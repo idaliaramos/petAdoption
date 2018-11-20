cab2170b996286569bf6cd8d6825054fafed7f31
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-11-19 22:09:34 -0800

    added on close function for modal

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 (() => {
   const onImageClick = () => {
+    //select all the images from the gallery
     const galleryImageList = document.querySelectorAll("#gallery li");
-    const galleryImages = [...galleryImageList];
-    galleryImages.forEach(image => {
+    console.log(galleryImageList, "gallery image list");
+    // const galleryImages = [...galleryImageList];
+    //add event listener to each of the images to open on click
+    galleryImageList.forEach(image => {
       image.addEventListener("click", event => {
         galleryImageOpen(event.target);
       });
     });
   };
   const galleryImageOpen = image => {
-    console.log(image, "image");
+    //get the image src
     const imageSrc = image.getAttribute("src");
+    //create a div that contains the image selected
     const openedImage = `<div class='backdrop'>
     <img src='${imageSrc}' alt="" />
     <span class='backdrop-close'> X </span>
   </div>`;
+    //insert the html into the body
     document.body.insertAdjacentHTML("beforeend", openedImage);
-    console.log(openedImage, "00000image");
+    galleryImageClose();
   };
 
+  const galleryImageClose = image => {
+    //get the close button
+    const closeButton = document.querySelector(".backdrop-close");
+    //add an event listener so onclick you get the whole backdrop and remove it
+    closeButton.addEventListener("click", () => {
+      const backdrop = document.querySelector(".backdrop");
+      backdrop.remove();
+    });
+  };
   onImageClick();
 })();
